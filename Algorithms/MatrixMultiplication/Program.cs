cd2be316f2c22fbf5c97cc8899725f1a62b4d2c1
alykov <alykov1995@gmail.com>
2016-10-16 02:38:45 +0700

    В алгоритмах удалены файлы, специфичные для IDE, оставлен только код

--- Algorithms/MatrixMultiplication/Program.cs
+++ Algorithms/MatrixMultiplication/Program.cs
@@ -12,46 +12,43 @@ class mainFrame : Work
     /// <summary>
     /// Реалиация алгоритма
     /// </summary>
+    public mainFrame(int port = 8002) : base(port) { }
     public override void slaveFun()
     {
         bool end = true;
         StreamReader R = new StreamReader("A.txt");
         int N = Convert.ToInt32(R.ReadLine());
         int ProcPartSize = N / getCount();
-        double[][] A = new double[ProcPartSize][];
+        float[][] A = new float[ProcPartSize][];
         for (int i = 0; i < ProcPartSize; i++)
         {
-            A[i] = new double[N];
+            A[i] = new float[N];
         }
-        double[][] B = new double[ProcPartSize][];
+        float[][] B = new float[ProcPartSize][];
         for (int i = 0; i < ProcPartSize; i++)
         {
-            B[i] = new double[N];
+            B[i] = new float[N];
         }
-        double[][] BuffC = new double[ProcPartSize][];
+        float[][] BuffC = new float[ProcPartSize][];
         for (int i = 0; i < ProcPartSize; i++)
         {
-            BuffC[i] = new double[N];
+            BuffC[i] = new float[N];
         }
-        double[][] buffer;
+        float[][] buffer;
         DateTime check;
         int pred = (getIndex() == 0) ? getCount() - 1 : getIndex() - 1;
         int next = (getIndex() == getCount() - 1) ? 0 : getIndex() + 1;
         DateTime time = System.DateTime.Now;
-        int ic = 0;
-        for (; ic < (N / getCount()) * getIndex(); R.ReadLine(), ic++); //Пропуск ненужных машине строк
         for (int i = 0; i < ProcPartSize; i++)
         {
-            A[i] = R.ReadLine().Split(' ').Select(Double.Parse).ToArray();
+            A[i] = R.ReadLine().Split(' ').Select(float.Parse).ToArray();
         }
         R.Close();
         R = new StreamReader("B.txt");
-        N = Convert.ToInt32(R.ReadLine());
-        ic = 0;
-        for (; ic < (N / getCount()) * getIndex(); R.ReadLine(), ic++) ;
+		R.ReadLine();
         for (int i = 0; i < ProcPartSize; i++)
         {
-            B[i] = R.ReadLine().Split(' ').Select(Double.Parse).ToArray();
+            B[i] = R.ReadLine().Split(' ').Select(float.Parse).ToArray();
         }
         R.Close();
         DateTime time1 = System.DateTime.Now;
@@ -59,7 +56,7 @@ class mainFrame : Work
         do
         {
             buffer = B;
-            double sum = 0;
+            float sum = 0;
             int ind;
             for (int m = 0; m < getCount(); m++)
             {
@@ -81,7 +78,7 @@ class mainFrame : Work
                 DiscretRecipientData R1 = GetData(pred);
                 DiscretSendData R2 = Send(next, buffer);
                 R1.block();
-                buffer = (double[][])R1.getData();
+                buffer = (float[][])R1.getData();
             }
             end = true;
         } while (!SGCJ(end)); //Точка синхронизации всех машин
@@ -89,34 +86,33 @@ class mainFrame : Work
         {
             CollectSendData cl = CollectorSend(0, BuffC);
             cl.block();
-            Console.WriteLine("CollectSend " + getIndex() + "part");
         }
         else
         {
-            double[][] C = new double[N][];
-            for (int i = 0; i < N; i++)
-                C[i] = new double[N];
-            for (int i = 0; i < ProcPartSize; i++)
-                C[i] = BuffC[i];
             CollectorRecipientData r = getAllData();
             r.block();
-            for (int i = 1; i < getCount(); i++)
-            {
-                double[][] data = (double[][])r.getData(i);
-                for (int k = 0; k < ProcPartSize; k++)
-                    C[i*ProcPartSize+k] = data[k];
-            }
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                    Console.Write(C[i][j] + " ");
-                Console.WriteLine();
-            } 
-        }
+			StreamWriter f = new StreamWriter("result.txt");
+			for (int i = 0; i < ProcPartSize; i++)
+			{
+				for (int j = 0; j < N; j++)
+					f.Write(BuffC[i][j]+" ");
+				f.WriteLine();
+			}
+			for (int i = 1; i < getCount(); i++)
+			{
+				float[][] data = (float[][])r.getData(i);
+				for (int j = 0; j < ProcPartSize; j++)
+				{
+					for (int k = 0; k < N; k++)
+						f.Write(data[j][k]+" ");
+					f.WriteLine();
+				}
+			}
         DateTime check1 = System.DateTime.Now;
-        Console.WriteLine("Reading time {0}", (time1 - time).TotalMilliseconds);
-        Console.WriteLine("Computing time {0}", (check1 - check).TotalMilliseconds);
-        Console.ReadKey();
+        f.WriteLine("Reading time {0} ms", (time1 - time).TotalMilliseconds);
+        f.WriteLine("Computing time {0} ms", (check1 - check).TotalMilliseconds);
+        f.Close();
+		}
     }
 }
 
@@ -127,7 +123,11 @@ class Program
 {
     public static void Main(string[] args)
     {
-        mainFrame m = new mainFrame();
+        mainFrame m;
+        if (args.Length == 0)
+            m = new mainFrame();
+        else
+            m = new mainFrame(Convert.ToInt32(args[1]));
         m.setSUP(0);
         m.start();
     }
