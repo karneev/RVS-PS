91374c309214efaca01263150d4a33f7af5245f7
alykov <alykov1995@gmail.com>
2016-10-16 02:57:56 +0700

    Добавлен генератор матриц для задачи их перемножения

---
+++ MatrixGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace MatrixGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            Random r = new Random();
+            StreamWriter a = new StreamWriter("A.txt");
+            StreamWriter b = new StreamWriter("B.txt");
+            a.WriteLine(N);
+            b.WriteLine(N);
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    a.Write(r.Next(100));
+                    if (j != N - 1) a.Write(" ");
+                    b.Write(r.Next(100));
+                    if (j != N - 1) b.Write(" ");
+                }
+                a.WriteLine();
+                b.WriteLine();
+            }
+            a.Close();
+            b.Close();
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
